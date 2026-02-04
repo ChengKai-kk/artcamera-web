78d2c3883900316be2909b75cea4316d3796bb69
成ck <1182969771@qq.com>
2026-02-04 22:04:35 +0800

    exe文件隐藏菜单栏

--- electron/main.cjs
+++ electron/main.cjs
@@ -79,6 +79,7 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 1280,
     height: 720,
+    autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, "preload.cjs"),
       contextIsolation: true,
